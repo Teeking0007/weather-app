f79892640503c70eef60c26b78b957060d470915
Teeking0007 <timmyther02@gmail.com>
2022-04-30 10:38:34 +0100

    update with country name and location current time fetching

--- src/App.js
+++ src/App.js
@@ -4,21 +4,30 @@ import axios from 'axios';
 
 function App() {
   const [data, setData] = useState({});
+  const [time, setTime] = useState({});
   const [location, setLocation] = useState ('');
 
   const apikey = '39afd92f191b4c928240b0fd8b1ddcd0';
  
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=Imperial&appid=${apikey}`
 
+  //const timeKey = 'db36a27c208c41938817ea58f2e5310e';
+   const baseURL = `https://timezone.abstractapi.com/v1/current_time/?api_key=fe7e55c47b8343f5a8f4072097f8c046&location=${location}`;
+
    const locationHandler = (e) => {
      e.preventDefault();
       axios.get(url).then(response=>{
         setData(response.data)
         console.log(response.data)
+      });
+      axios.get(baseURL).then(response=>{
+        console.log(response.data)
+        setTime(response.data)
       })
      setLocation('')
     
    }
+   
   
 
   return (
@@ -36,7 +45,7 @@ function App() {
       <div className='container'>
         <div className='top'>
           <div>
-            <h2 className='location'>{data? data.name : null}</h2>
+            <h2 className='location'>{data? data.name : null}, {data.sys? data.sys.country : null}</h2>
             <h1 className='temp'>{data.main? data.main.temp: null} &#8457;</h1>
 
           </div>
@@ -44,6 +53,9 @@ function App() {
               <h4>{data.weather? data.weather[0].main : null}</h4>
           </div>
           
+        </div>
+        <div className='middle'>
+            <h2>{time? time.datetime : null}</h2>
         </div>
         <div className='buttom'>
           <div className='bottom-container'>
